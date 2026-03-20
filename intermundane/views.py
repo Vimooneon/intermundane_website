c5ccc2a3e99d8988974d9d9521117a63d1773515
vimooneon@gmail.com <vimooneon@gmail.com>
2026-03-20 16:39:18 +0000

    added html template

--- intermundane/views.py
+++ intermundane/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+#from django.http import HttpResponse
+#from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "intermundane/intermundane.html")
+
+    #template = loader.get_template("intermundane/intermundane.html")
+    #return HttpResponse(template.render())
